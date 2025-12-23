078e61f84bf4ac8f240c672c877e65089732de6b
T.Watanabe <watanabe@colorfulcompany.co.jp>
2025-12-23 15:59:45 +0900

    feat(simple-backstage-status): add logger to SimpleBackstageStatus::File::LoaderBase

--- packages/simple-backstage-status/lib/simple_backstage_status/file/interval_reader.rb
+++ packages/simple-backstage-status/lib/simple_backstage_status/file/interval_reader.rb
@@ -7,15 +7,20 @@ module SimpleBackstageStatus
       #
       # @param [Numeric] ttl
       # @param [File] file
+      # @param [Logger] logger
       #
-      def initialize(ttl: TTL, file: ::File)
+      def initialize(ttl: TTL, file: ::File, logger: nil)
         # @return [Numeric]
         @ttl = ttl
         # @return [File]
         @file = file
         # @return [Hash]
         @cache = {}
+
+        @logger = logger
       end
+      # @return [Logger]
+      attr_reader :logger
 
       #
       # @param [String] path
@@ -28,9 +33,12 @@ module SimpleBackstageStatus
         if !@cache[path] || !@cache.dig(path, :last_read_at) || @cache.dig(path, :last_read_at) + @ttl <= now
           c = {}
           c[:content] = @file.read(path)
+          logger&.info(self, "#{path} has read.")
           c[:last_read_at] = now
 
           @cache[path] = c
+        else
+          logger&.info(self, "#{path} cache hit.")
         end
 
         @cache.dig(path, :content)
